22d96248d1d5ba14156cec44ad501cfbea0236c2
Gammelinne <kylian.renault@viacesi.fr>
2021-12-05 21:53:41 +0100

    Ajoutez des fichiers projet.

---
+++ Projet/Ville.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "pch.h"
+namespace NS_BDD
+{
+    ref class Ville
+    {
+    private:
+        System::String^ id_ville;
+        System::String^ nom;
+        System::String^ code_postal;
+
+    public:
+        void set_id_ville(System::String^);
+        void set_nom(System::String^);
+        void set_code_postal(System::String^);
+
+        System::String^ get_id_ville(void);
+        System::String^ get_nom(void);
+        System::String^ get_code_postal(void);
+    };
+}
